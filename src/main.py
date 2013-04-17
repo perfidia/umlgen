47f0fb22f4a78db011572381bdf2a3b0e4a2e52a
Jakub Wojtczak <jjwojtczak@gmail.com>
2013-04-17 17:45:37 +0200

    Model of context umlgen

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from model import *
+from simple_visitor import *
+
+def main():
+    entity_1 = Entity("id_1", "one", ENTITY_TYPE_HUMAN)
+    entity_2 = Entity("id_2", "two", ENTITY_TYPE_SYSTEM)
+    
+    process = Process("p_id", "label")
+    process.add_child(entity_1)
+    process.add_child(entity_2)
+    
+    context = ContextDiagram(process)
+    
+    return
+
+if __name__ == "__main__":
+    main()
+    
---
+++ src/simple_visitor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class SimpleVisitor(object):
+    def __init__(self):
+        pass
+    
+    def visit_context_diagram(self, context_diagram):
+        pass
+    
+    def visit_process(self, process):
+        pass
+    
+    def visit_entity(self, entity):
+        pass
