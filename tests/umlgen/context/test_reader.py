2ae3876c4db05bd581c3548657de39fc1e828262
matkra <krawczo3d@o2.pl>
2013-04-24 20:27:50 +0200

    XMLReader tests

---
+++ tests/umlgen/context/test_reader.py
@@ -0,0 +1,62 @@
+'''
+Created on Apr 24, 2013
+
+@author: Mateusz Krawczynski
+'''
+import unittest
+from reader import XMLReader
+from model import *
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.reader = XMLReader("../context/example.xml")
+        self.diagram = self.reader.create_graph()
+
+
+
+    def testEntitiesId(self):
+        temp = {}
+        temp[0] = "0"
+        temp[1] = "e_1"
+        temp[2] = "p_0"
+        temp[3] = "e_2"
+        i = 0
+        for entity in self.diagram.all_entities:
+            self.assertEqual(entity, temp[i])
+            i+=1
+
+    def testConnections(self):
+        for entity_id in self.diagram.all_entities:
+            entity = self.diagram.all_entities[entity_id]
+            children = entity.get_children()
+            for entityid in children:
+                self.assertEqual(entityid.get("connection").get_label(), "a connection")
+                
+    def testConnectionEntities(self):
+        for entity_id in self.diagram.all_entities:
+            entity = self.diagram.all_entities[entity_id]
+            children = entity.get_children()
+            temp = {}
+            temp[0] = "e_1"
+            temp[1] = "0"
+            i = 0
+            for entityid in children:
+                self.assertEquals(entityid.get("child").get_id(), temp[i])
+                i+=1
+             
+            
+    def testProcessLabel(self):
+            self.assertEqual(self.diagram._process.get_label(), "system")
+            
+    def testProcessId(self):
+        self.assertEqual(self.diagram._process.get_id(), "p_0")
+
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
