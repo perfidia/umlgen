2c2771e14a535230eecfec540fcf5684009ca310
Jakub Wojtczak <jjwojtczak@gmail.com>
2013-04-24 13:24:21 +0200

    reading from xml file

--- src/main.py
+++ src/main.py
@@ -3,17 +3,8 @@
 from umlgen import *
 
 def main():
-    entity_1 = Entity("id_1", "one", ENTITY_TYPE_HUMAN)
-    entity_2 = Entity("id_2", "two", ENTITY_TYPE_SYSTEM)
-    
-    process = Process("p_id", "label")
-    process.add_child(entity_1, Connection("reads"))
-    process.add_child(entity_2, Connection("reads"))
-    
-    entity_1.add_child(entity_2, Connection("is readed by"))
-    entity_2.add_child(entity_1, Connection("is readed by"))
-    
-    context = ContextDiagram(process)
+    r = XMLReader("../context/example.xml")
+    context = r.create_graph()
     
     v = SimpleVisitor()
     context.accept(v)
--- src/umlgen/context/reader.py
+++ src/umlgen/context/reader.py
@@ -1,10 +1,63 @@
 #!/usr/bin/env python
 
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
+from model import *
 
 class XMLReader(object):
     def __init__(self, path):
         self._path = path
+        self._tree = ET.parse(path)
         
     def create_graph(self):
-        return None
+        root = self._tree.getroot()
+        p = self._get_process(root)
+        entities = self._get_entities(root)
+        entities[p.get_id()] = p
+        self._make_connections(root, entities)
+        c = ContextDiagram(p)
+        
+        return c
+    
+    def _make_connections(self, root, entities):
+        connections_tag = root.find("connections")
+        
+        for tag in connections_tag.findall("connection"):
+            label = tag.find("label").text.strip()
+            c_from = tag.find("from").text.strip()
+            c_to = tag.find("to").text.strip()
+            conn = Connection(label)
+            
+            entities[c_from].add_child(entities[c_to], conn)
+    
+    def _get_entities(self, root):
+        entities_tag = root.find("entities")
+        entities = {}
+        
+        for tag in entities_tag.findall("entity"):
+            label = tag.find("label").text.strip()
+            eid = tag.find("id").text.strip()
+            etype = tag.find("type").text.strip()
+            
+            e_type = ENTITY_TYPE_SYSTEM
+            if etype == "human":
+                e_type = ENTITY_TYPE_HUMAN
+            
+            entity = Entity(eid, label, e_type)
+            
+            entities[eid] = entity
+            
+        return entities
+    
+    def _get_process(self, root):
+        tag = root.find("process")
+        label = tag.find("label").text.strip()
+        pid = tag.find("id").text.strip()
+        return Process(pid, label)
+    
+    
+    
+    
+    
+    
+    
+    
